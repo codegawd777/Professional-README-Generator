e7f9965fed0c13dd147d425af2e686ac1e86c50a
JustinHinton <justgawd7@icloud.com>
2024-05-26 13:57:17 -0600

    added license link function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,17 @@ function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (license==="APACHE 2.0") {
+  return `[${license}](http://www.apache.org/licenses/)`  
+  }
+  else if (license==="MIT") {
+    return `[${license}](https://choosealicense.com/licenses/mit/)`
+  }
+  else if (license==="None") {
+    return ``
+  }
+ }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
