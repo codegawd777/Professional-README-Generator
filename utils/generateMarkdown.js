6899ca4e84be4224c7c72325c3fccc7c93f1b6dd
JustinHinton <justgawd7@icloud.com>
2024-05-26 13:52:31 -0600

    added license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,7 +8,17 @@ function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (license==="APACHE 2.0") {
+  return `A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`  
+  }
+  else if (license==="MIT") {
+    return `A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
+  }
+  else if (license==="None") {
+    return ``
+  }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
